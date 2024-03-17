6b62d488fcc84b53a3e71ca05c0fcb8aca24b3eb
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-03-16 22:26:18 -0500

    improve the view of the dynamic categories

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,8 +1,36 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Programacion',
+	'Inversion',
+	'Tecnologia financiera',
+	'Educacion',
+	'Experiencia'
 ] as const
+
+export const categorias = [
+	'Programación',
+	'Inversión',
+	'Tecnología financiera',
+	'Educación',
+	'Experiencia'
+] as const
+
+export const lookingForCategory = async (x: string, lx: Readonly<string[]>) => {
+	x.toLocaleLowerCase()
+	let i: number
+	for (i = 0; i < lx.length; i++) {
+		if (x == lx[i].toLocaleLowerCase()) {
+			return i
+		}
+	}
+	return -1
+}
+export const lookingForCategoryNormal = async (x: string, lx: Readonly<string[]>) => {
+	let i: number
+	for (i = 0; i < lx.length; i++) {
+		if (x == lx[i]) {
+			return i
+		}
+	}
+	return -1
+}
