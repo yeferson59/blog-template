501d039c5b515f277e1f49ebc2c0271771493136
yefersonT <116679774+yeferson59@users.noreply.github.com>
2024-03-26 20:50:36 -0500

    changed urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import { remarkReadingTime } from './src/utils/readTime.ts'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.tecnofinanzas.blog', // Write here your website url
+	site: 'https://tecnofinanzas.blog', // Write here your website url
 	markdown: {
 		remarkPlugins: [remarkReadingTime],
 		drafts: true,
